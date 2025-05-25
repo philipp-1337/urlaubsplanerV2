17bdb0c9fc543531da713e57967feff938b6160d
Philipp Kanter <philippkanter@gmail.com>
2025-05-25 13:45:48 +0200

    footer link fix

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext'; // Importiere useAuth
+
 
 const Footer = () => {
+  const { isLoggedIn } = useAuth(); // Hole den Login-Status
+  const location = useLocation(); // Hole die aktuelle Location
+
   return (
     <footer className="bg-gray-100 text-center py-4">
       <div className="text-xs text-gray-500">
-        {/* <Link to="/" className="hover:text-primary px-2">
-          Startseite
-        </Link>
-        <span className="select-none">|</span> */}
-        {/* <Link to="/impressum" className="hover:text-primary px-2">
-          Impressum
-        </Link>
-        <span className="select-none">|</span> */}
+        {/* Zeige Login-Link nur, wenn nicht eingeloggt und nicht auf der Login-Seite */}
+        {!isLoggedIn && location.pathname !== '/login' && (
+          <>
+            <Link to="/login" className="hover:text-primary px-2">
+              Login
+            </Link>
+            <span className="select-none">|</span>
+          </>
+        )}
         <Link to="/datenschutz" className="hover:text-primary px-2">
           Datenschutzerklärung
         </Link>
+        {/* <span className="select-none">|</span>
+        <Link to="/impressum" className="hover:text-primary px-2">
+          Impressum
+        </Link> */}
       </div>
     </footer>
   );
