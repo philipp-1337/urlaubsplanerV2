eaf97ad698ba5cd9366cf0b045fca53cb943683f
Philipp Kanter <philippkanter@gmail.com>
2025-05-25 09:02:52 +0200

    sw: craco config adjusted

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,29 @@
-// craco.config.js
+const { GenerateSW } = require('workbox-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-  // Beispiel: Customize Webpack oder Babel hier
-  // customizeWebpackConfig: {},
+  webpack: {
+    // alias: {
+    //   "@utils": path.resolve(__dirname, "src/utils"),
+    //   "@components": path.resolve(__dirname, "src/components"),
+    //   "@hooks": path.resolve(__dirname, "src/hooks"),
+    //   "@constants": path.resolve(__dirname, "src/constants")
+    // },
+    configure: (webpackConfig) => {
+      // Nur im Production-Build das Plugin hinzufügen
+      if (process.env.NODE_ENV === 'production') {
+        webpackConfig.plugins.push(
+          new GenerateSW({
+            clientsClaim: true, // Wichtig: Damit der neue SW die Kontrolle übernimmt
+            skipWaiting: true,  // Wichtig: Aktiviert den neuen SW sofort (oder nach Nachricht)
+            maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+            include: [/\.html$/, /\.js$/, /\.css$/, /\.png$/, /\.jpg$/, /\.svg$/],
+            // Stelle sicher, dass der generierte SW auf die Nachricht hört
+            // Workbox v6+ macht dies standardmäßig, wenn skipWaiting: true gesetzt ist.
+          })
+        );
+      }
+      return webpackConfig;
+    }
+  }
 };
